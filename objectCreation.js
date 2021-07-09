d3920686539fdfea3fc09d5f9a2246c5f06613ef
ankit@tomhorngaming.com <you@example.com>
2021-07-09 20:48:17 +0530

    Added more methods on objects creation.

--- objectCreation.js
+++ objectCreation.js
@@ -1,12 +1,23 @@
+
+//Type 1
 var tv = new Object();
 tv.screentype = "hd";
 tv.manufacturer = "onida";
 tv.price = 200;
 console.log(tv);
 
+//Type 2
 var radio = {
     brand : "onida",
     price : 200
 }
 
 console.log(radio);
+
+//Object Constructor
+function Course(name,duration){
+    this.name = name;
+    this.duration = duration;
+}
+
+console.log(new Course("Learns JS",5))
