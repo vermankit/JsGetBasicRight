ffb0a42040bc009ea45a309927c31eff0fb4b74e
ankit@tomhorngaming.com <you@example.com>
2021-07-10 12:17:04 +0530

    Added constants concepts

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # JsGetBasicRight
 
 Refresh your basic concept of JavaScript.
+
+How to run a code
+install node on your machine.
+
+Open the file folder in cmd
+
+//Run file by below command
+node {filename}  
--- constant.js
+++ constant.js
@@ -1,18 +1,55 @@
-const pie = 3.14;
+function constantScope(arg){
+    const pie = 3.14;
+    if(arg){
+        try {
+            const pie = 3.1444;
+            pie = arg;  //Reassigment of constant is not possible
+        } catch (error) {
+            console.log(error.message);
+        }      
+    }
+    console.log(pie);
+}
+
+constantScope(2); // This will print error message then value of pie.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const array = [1,2,3,4];
-try {
-    pie = 333; 
-} catch (error) {
-    console.log(error);
+
+function reassignArray(arr){
+    try {
+        array = arr;  //This will throw error as reassigment is not possible
+   } catch (error) {
+       console.log(error);
+   } 
+}
+
+reassignArray([5,6,7,8]);
+
+function pushArray(item){
+    try {
+        array.push(item); // this is possible as we are adding content not reassigning array
+        array[1] = 200;   // this is possible as we are adding content not reassigning array
+        console.log(array);
+   } catch (error) {
+       console.log(error);
+   } 
+}
+
+pushArray(5);
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+///////////////////////////////////Example 3/////////////////////////////////////////////////
+const radio = {
+    name : "boat"
 }
 
 try {
-     array.push(5);   
-     console.log(array);
+    radio.name = "carvaan";  
+    radio.price = 300;
+    //This is also possible as we are not reassigning to radio object
+    console.log(radio);
 } catch (error) {
     console.log(error);
 }
-
-
-
-
